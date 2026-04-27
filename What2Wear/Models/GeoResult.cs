86f4248f7c3b1b97bd0f5672d93cf8c9bb9639bf
ArGul <argul.mail@gmail.com>
2026-04-27 11:28:15 +0300

    Created GeoResult

---
+++ What2Wear/Models/GeoResult.cs
@@ -0,0 +1,8 @@
+namespace What2Wear.Models;
+
+public class GeoResult
+{
+    public string NameOfCity { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
