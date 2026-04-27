070d9bfc0b7ab7989aca6c9eb09c4e0989ba306a
ArGul <argul.mail@gmail.com>
2026-04-27 11:35:25 +0300

    Not GeoResult city have name "Name"

--- What2Wear/Models/GeoResult.cs
+++ What2Wear/Models/GeoResult.cs
@@ -2,7 +2,7 @@ namespace What2Wear.Models;
 
 public class GeoResult
 {
-    public string NameOfCity { get; set; }
+    public string Name { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
 }
